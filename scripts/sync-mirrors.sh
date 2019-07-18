e7530f9045cc68ec0ff1e7a704188cf4c66c5f63
Patryk Obara <dreamer.tan@gmail.com>
2019-07-18 16:21:20 +0200

    Add mirroring script

---
+++ scripts/sync-mirrors.sh
@@ -0,0 +1,49 @@
+#!/bin/env bash
+
+# Setup working copies for projects mirrored by Luxtorpeda project.
+# Perform one way sync (origin → gitlab) of master branches.
+
+lowercase () {
+	echo "$@" | tr '[:upper:]' '[:lower:]'
+}
+
+repo_name () {
+	url=$1
+	lowercase "$(basename "${url%.git}")"
+}
+
+readonly ioq3_url=git@github.com:ioquake/ioq3.git
+readonly openxcom_url=git@github.com:OpenXcom/OpenXcom.git
+
+readonly all_projects="$ioq3_url $openxcom_url"
+
+# set -x
+
+cd "$(git rev-parse --show-toplevel)" || exit
+if [ ! -d ../mirrors ] ; then
+	mkdir -p ../mirrors
+fi
+cd ../mirrors || exit
+
+# initializing:
+
+for project_url in $all_projects ; do
+	repo_name="$(repo_name "$project_url")"
+	mirror_url=git@gitlab.com:luxtorpeda/mirrors/${repo_name}.git
+	if [ -d "$repo_name" ] ; then
+		continue
+	fi
+	echo "Cloning $project_url"
+	git clone "$project_url" "$repo_name"
+	git -C "$repo_name" remote add gitlab "$mirror_url"
+ 	git -C "$repo_name" push gitlab master
+done
+
+# syncing
+
+for project_url in $all_projects ; do
+	repo_name="$(repo_name "$project_url")"
+	echo "Syncing $repo_name"
+	git -C "$repo_name" fetch --all
+	git -C "$repo_name" push --force gitlab origin/master:master
+done
