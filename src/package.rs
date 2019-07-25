cee57a505d6aa8076b9d88674bf75ea4dfdaaffe
Patryk Obara <dreamer.tan@gmail.com>
2019-07-25 10:05:58 +0200

    Implement download for a specific package

--- src/package.rs
+++ src/package.rs
@@ -1,5 +1,6 @@
 extern crate reqwest;
 
+use std::fs;
 use std::io;
 use std::io::{Error, ErrorKind};
 use std::path::PathBuf;
@@ -7,19 +8,38 @@ use std::process::Command;
 
 use crate::user_env;
 
-fn find_cached_file(app_id: &String, file: &String) -> Option<PathBuf> {
+fn place_cached_file(app_id: &str, file: &str) -> io::Result<PathBuf> {
+    let xdg_dirs = xdg::BaseDirectories::new().unwrap();
+    let path_str = format!("luxtorpeda/{}/{}", app_id, file);
+    xdg_dirs.place_cache_file(path_str)
+}
+
+fn find_cached_file(app_id: &str, file: &str) -> Option<PathBuf> {
     let xdg_dirs = xdg::BaseDirectories::new().unwrap();
     let path_str = format!("luxtorpeda/{}/{}", app_id, file);
     xdg_dirs.find_cache_file(path_str)
 }
 
-pub fn is_cached(app_id: &String) -> bool {
-    find_cached_file(app_id, &"dist.tar.xz".to_string()).is_some()
+pub fn is_cached(app_id: &str) -> bool {
+    find_cached_file(app_id, "dist.tar.xz").is_some()
 }
 
-pub fn download(app_id: &String) -> io::Result<()> {
-    println!("downloading {:}", app_id);
-    Ok(())
+pub fn download(app_id: &str) -> io::Result<()> {
+    println!("downloading package for app_id {:}", app_id);
+
+    let target = "https://luxtorpeda.gitlab.io/packages/ioq3/dist.tar.xz";
+    match reqwest::get(target) {
+        Ok(mut response) => {
+            let dest_file = place_cached_file(app_id, "test.tar.xz")?;
+            let mut dest = fs::File::create(dest_file)?;
+            io::copy(&mut response, &mut dest)?;
+            Ok(())
+        }
+        Err(err) => {
+            println!("download err: {:?}", err);
+            Err(Error::new(ErrorKind::Other, "download error"))
+        }
+    }
 }
 
 pub fn install(package: String) -> io::Result<()> {
