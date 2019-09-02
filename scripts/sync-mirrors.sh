b78529326a55ccad0211515cb8dd0f8d521556f4
Patryk Obara <dreamer.tan@gmail.com>
2019-09-02 02:14:53 +0200

    Simplify mirror list

--- scripts/sync-mirrors.sh
+++ scripts/sync-mirrors.sh
@@ -12,20 +12,21 @@ repo_name () {
 	lowercase "$(basename "${url%.git}")"
 }
 
-readonly ags_url=git@github.com:adventuregamestudio/ags.git
-readonly dhewm3_url=git@github.com:dhewm/dhewm3.git
-readonly eternaljk_url=git@github.com:eternalcodes/EternalJK.git
-readonly gzdoom_url=git@github.com:coelckers/gzdoom.git
-readonly ioq3_url=git@github.com:ioquake/ioq3.git
-readonly iortcw_url=git@github.com:iortcw/iortcw.git
-readonly openjk_url=git@github.com:JACoders/OpenJK.git
-readonly openmw_url=git@gitlab.com:OpenMW/openmw.git
-readonly openrct2_url=git@github.com:OpenRCT2/OpenRCT2.git
-readonly openxcom_url=git@github.com:OpenXcom/OpenXcom.git
-readonly regoth_url=git@github.com:REGoth-project/REGoth-bs.git
-readonly vkquake_url=git@github.com:Novum/vkQuake.git
-
-readonly all_projects="$ags_url $dhewm3_url $eternaljk_url $gzdoom_url $ioq3_url $iortcw_url $openjk_url $openmw_url $openrct2_url $openxcom_url $regoth_url $vkquake_url"
+all_projects () {
+	echo git@github.com:adventuregamestudio/ags.git
+	echo git@github.com:arx/ArxLibertatis.git
+	echo git@github.com:coelckers/gzdoom.git
+	echo git@github.com:dhewm/dhewm3.git
+	echo git@github.com:eternalcodes/EternalJK.git
+	echo git@github.com:ioquake/ioq3.git
+	echo git@github.com:iortcw/iortcw.git
+	echo git@github.com:JACoders/OpenJK.git
+	echo git@github.com:Novum/vkQuake.git
+	echo git@github.com:OpenRCT2/OpenRCT2.git
+	echo git@github.com:OpenXcom/OpenXcom.git
+	echo git@github.com:REGoth-project/REGoth-bs.git
+	echo git@gitlab.com:OpenMW/openmw.git
+}
 
 # set -x
 
@@ -38,7 +39,7 @@ echo "Using dir: $(pwd)"
 
 # initializing:
 
-for project_url in $all_projects ; do
+for project_url in $(all_projects) ; do
 	repo_name="$(repo_name "$project_url")"
 	mirror_url=git@gitlab.com:luxtorpeda/mirrors/${repo_name}.git
 	if [ -d "$repo_name" ] ; then
@@ -54,7 +55,7 @@ done
 
 # syncing
 
-for project_url in $all_projects ; do
+for project_url in $(all_projects) ; do
 	repo_name="$(repo_name "$project_url")"
 	echo "Syncing $repo_name"
 	git -C "$repo_name" fetch --all
