486e9e5bf257e5fb52fbf4ae667c9778081fcd18
Patryk Obara <dreamer.tan@gmail.com>
2019-09-10 11:39:04 +0200

    Show package status compared to release branches

--- scripts/packages-status.sh
+++ scripts/packages-status.sh
@@ -17,11 +17,23 @@ parse_sub_status () {
 	[[ "$*" =~ .([0-9a-f]+) ]] && echo "${BASH_REMATCH[1]}"
 }
 
+log_new_tags () {
+	local -r branches=$1
+	for b in $branches ; do
+		git -C "../mirrors/$mirror/" log \
+			--decorate \
+			--pretty="format:%h %d" \
+			"$pkg_release_version..origin/$b" \
+			| grep '(tag:'
+	done
+}
+
 cd "$(git rev-parse --show-toplevel)" || exit
 
 while read -r line ; do
 	pkg=$(echo "$line" | cut -d '|' -f1 | tr -d '[:space:]')
 	proj=$(echo "$line" | cut -d '|' -f2 | tr -d '[:space:]')
+	branches=$(echo "$line" | cut -d '|' -f3)
 	mirror=$(repo_name "$proj")
 
 	if [ -z "$pkg" ] ; then
@@ -47,11 +59,7 @@ while read -r line ; do
 		"$pkg_release_version..$mirror_version" | wc -l
 
 	echo "new tags:"
-	git -C "../mirrors/$mirror/" log \
-		--decorate \
-		--pretty="format:%h %d" \
-		"$pkg_release_version..$mirror_version" \
-		| grep '(tag:'
+	log_new_tags "$branches" | sort -u
 
 	echo
 done < scripts/packages.txt
