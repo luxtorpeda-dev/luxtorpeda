04547a4d96f66210cdd2c5a4c32496e46d12c072
d10sfan <d10sfan@gmail.com>
2022-05-06 20:39:17 -0500

    drop

--- src/ui.rs
+++ src/ui.rs
@@ -409,6 +409,7 @@ impl EguiWindowInstance {
             if exit || window_data.should_close {
                 *control_flow = glutin::event_loop::ControlFlow::Exit;
                 egui_glow.destroy();
+                drop(gl_window.window());
             }
 
             if window_data.reload_requested {
