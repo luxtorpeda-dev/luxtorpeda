b6b51eab00989c8a69ff624f25fc2a487f355b54
d10sfan <d10sfan@gmail.com>
2022-11-13 11:48:17 -0600

    Ignore iscriptevaluator earlier in the bash script, to avoid Godot window opening

--- luxtorpeda.sh
+++ luxtorpeda.sh
@@ -8,4 +8,9 @@ fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [[ $2 == *"iscriptevaluator.exe"* ]]; then
+  echo "ignoring iscriptevaluator.exe"
+  exit 0
+fi
+
 "$DIR/luxtorpeda" "$@"
